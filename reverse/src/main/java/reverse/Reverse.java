d3614e5aa727ce91aa202f2b37846da11ca4f554
ajay naveen <ajay naveen@192.168.0.5>
2021-05-21 17:15:32 +0530

    rev

---
+++ reverse/src/main/java/reverse/Reverse.java
@@ -0,0 +1,18 @@
+package reverse;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		int num = 6942, reversed = 0;
+
+	    while(num != 0) {
+	    
+	      int digit = num % 10;
+	      reversed = reversed * 10 + digit;
+	      num /= 10;
+	    }
+
+	    System.out.println("Reversed Number: " + reversed);
+	}
+
+}
